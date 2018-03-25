c3e8163fee61bb872a565f94c5c16848914d224d
trihandayani97 <36077482+trihandayani97@users.noreply.github.com>
2018-03-25 15:55:34 +0700

    init commit

---
+++ app/src/main/java/com/example/trihandayani/trihandayani_1202154321_modul5/AddToDoList.java
@@ -0,0 +1,13 @@
+package com.example.trihandayani.trihandayani_1202154321_modul5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddToDoList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_to_do_list);
+    }
+}
